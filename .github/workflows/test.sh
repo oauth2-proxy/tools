3b023b3834780b86ca971e1b384f208c4b5a5c58
Joel Speed <joel.speed@hotmail.co.uk>
2022-02-17 16:15:36 +0000

    Update test script to work with multiple tools

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -2,25 +2,34 @@
 # manually exiting from script, because after-build needs to run always
 set +e
 
-if [ -z $CC_TEST_REPORTER_ID ]; then
-  echo "1. CC_TEST_REPORTER_ID is unset, skipping"
-else
-  echo "1. Running before-build"
-  ./cc-test-reporter before-build
-fi
+tools=reference-gen
+test_reporter=$PWD/cc-test-reporter
 
-echo "2. Running test"
-make test
-TEST_STATUS=$?
+for tool in ${tools}; do
+  pushd ${tool}
 
-if [ -z $CC_TEST_REPORTER_ID ]; then
-  echo "3. CC_TEST_REPORTER_ID is unset, skipping"
-else
-  echo "3. Running after-build"
-  ./cc-test-reporter after-build --exit-code $TEST_STATUS -t gocov --prefix $(go list -m)
-fi
+  if [ -z $CC_TEST_REPORTER_ID ]; then
+    echo "1. CC_TEST_REPORTER_ID is unset, skipping"
+  else
+    echo "1. Running before-build"
+    ${test_reporter} before-build
+  fi
 
-if [ "$TEST_STATUS" -ne 0 ]; then
-  echo "Test failed, status code: $TEST_STATUS"
-  exit $TEST_STATUS
-fi
+  echo "2. Running test"
+  make test
+  TEST_STATUS=$?
+
+  if [ -z $CC_TEST_REPORTER_ID ]; then
+    echo "3. CC_TEST_REPORTER_ID is unset, skipping"
+  else
+    echo "3. Running after-build"
+    ${test_reporter} after-build --exit-code $TEST_STATUS -t gocov --prefix $(go list -m)
+  fi
+
+  if [ "$TEST_STATUS" -ne 0 ]; then
+    echo "Test failed, status code: $TEST_STATUS"
+    exit $TEST_STATUS
+  fi
+
+  popd
+done
